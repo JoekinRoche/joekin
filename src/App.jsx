3950851f0ea151d051933c0fb4aa11b1edb61af6
Joekin Roche <joekinroches@gmail.com>
2023-02-17 12:06:40 +0530

    Assets added

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 // import Header from './components/header/Header'
-import Nav from './Components/nav/nav'
-import About from './Components/about/about'
-import Contact from './Components/contact/contact'
-import Experience from './Components/experience/experience'
-import Projects from './Components/project/project'
-import Services from './Components/services/services'
-import Testimonials from './Components/testimonials/testimonials'
-import Footer from './Components/footer/footer'
+import Nav from './Components/nav/Nav'
+import About from './Components/about/About'
+import Contact from './Components/contact/Contact'
+import Experience from './Components/experience/Experience'
+import Projects from './Components/project/Project'
+import Services from './Components/services/Services'
+import Testimonials from './Components/testimonials/Testimonials'
+import Footer from './Components/footer/Footer'
 
 const App = () => {
   return (
