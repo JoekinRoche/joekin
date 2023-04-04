ff2c581b8ee3a110b17bfe3e650f4b0237b5558c
Joekin Roche <joekinroches@gmail.com>
2023-04-04 16:24:52 +0530

    Fix nav

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 // import Header from './components/header/Header'
-import Nav from './Components/nav/Nav'
+import Nav from './Components/nav/nav'
 import About from './Components/about/about'
 import Contact from './Components/contact/Contact'
 import Experience from './Components/experience/Experience'
