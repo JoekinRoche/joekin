37091acaf7e3a5e3462082288b0136a7922b8a41
Joekin Roche <joekinroches@gmail.com>
2023-04-04 16:30:12 +0530

    Fix more inconsistencies

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,12 @@ import React from 'react'
 // import Header from './components/header/Header'
 import Nav from './Components/nav/nav'
 import About from './Components/about/about'
-import Contact from './Components/contact/Contact'
-import Experience from './Components/experience/Experience'
+import Contact from './Components/contact/contact'
+import Experience from './Components/experience/experience'
 import Projects from './Components/project/Projects'
 import Services from './Components/services/Services'
 import Testimonials from './Components/testimonials/Testimonials'
-import Footer from './Components/footer/Footer'
+import Footer from './Components/footer/footer'
 import { motion, useScroll } from "framer-motion"
 
 
